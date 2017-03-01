5575e9d9b4f2d67500dbaa4109b7622f74a0e446
yidanlin <137763911@qq.com>
2017-03-01 16:42:30 +0800

    测试rredis

---
+++ src/main/java/com/tianan/mapper/ProductMapper.java
@@ -0,0 +1,14 @@
+package com.tianan.mapper;
+
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.tianan.model.Product;
+
+/**
+ *
+ * Product 表数据库控制层接口
+ *
+ */
+public interface ProductMapper extends BaseMapper<Product> {
+
+}
